1964fce98ed4ee692ee785c1dd4fea110ba3b0f5
Kemot <tomasz.budzynski.programista@gmail.com>
2020-11-14 22:56:46 +0100

    Added MVC framework structure

---
+++ App/Config.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * PHP version 7.0
+ */
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'mvclogin';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'mvcuser';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = 'secret';
+
+    /**
+     * Show or hide error messages on screen
+     * @var boolean
+     */
+    const SHOW_ERRORS = true;
+    
+    /**
+     * Secret key for hashing
+     * @var boolean
+     */
+    const SECRET_KEY = 'd7oXp566nhN7p1SguOkVZb6VsC2zNgGH';
+    
+    /**
+     * Mailgun Api key
+     * @var string
+     */
+    const MAILGUN_API_KEY = '27392fcff077052f6f671d757105cc98-ba042922-effb5305';
+    
+    /**
+     * Mailgun domain name
+     * @var string
+     */
+    const MAILGUN_DOMAIN = 'sandbox512042a505894bc0a43f4ac2a6d5d0cf.mailgun.org';
+}
---
+++ App/Mail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use App\Config;
+use Mailgun\Mailgun;
+
+/**
+ * Mail
+ *
+ * PHP version 7.0
+ */
+class Mail
+{
+
+    /**
+     * Send a message
+     *
+     * @param string $to Recipient
+     * @param string $subject Subject
+     * @param string $text Text-only content of the message
+     * @param string $html HTML content of the message
+     *
+     * @return mixed
+     */
+    public static function send($to, $subject, $text, $html)
+    {
+        $mg = Mailgun::create(Config::MAILGUN_API_KEY);
+        $domain = Config::MAILGUN_DOMAIN;
+
+        $mg->messages()->send($domain, [
+            'from'    => 'kemot@test.com',
+            'to'      => $to,
+            'subject' => $subject,
+            'text'    => $text,
+            'html'    => $html]);
+    }
+}
