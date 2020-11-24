e458d52a9a5f14640dbba5b15f000d6b08e3614c
Kemot <tomasz.budzynski.programista@gmail.com>
2020-11-24 22:15:59 +0100

    Add selecting budget period besides custom dates

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -35,8 +35,30 @@ class Balance extends Authenticated
      */
    public function showAction()
    {
+       $balancePeriod = 1; //default balance perion 1 = current month
+       if(isset($_POST['balance_period'])){
+           $balancePeriod = $_POST['balance_period'];
+       }
        
-       $incomeSumsByCategory = Incomes::getSumsGroupedByCategory($this->user->id);
+       switch($balancePeriod){
+           case 1:
+               $balanceHeader = 'bierzący miesiąc';
+            break;
+               
+           case 2:
+               $balanceHeader = 'poprzedni miesiąc';
+            break;
+               
+           case 3:
+               $balanceHeader = 'bierzący rok';
+            break;
+               
+           default:
+               $balanceHeader = 'cos poszlo nie tak';
+            break; 
+       }
+       
+       $incomeSumsByCategory = Incomes::getSumsGroupedByCategory($this->user->id, $balancePeriod);
        
        $incomesSum = 0;
        
@@ -46,7 +68,7 @@ class Balance extends Authenticated
        
        //$incomesSum = number_format($incomesSum, 2, '.', ' ');
        
-       $expenseSumsByCategory = Expenses::getSumsGroupedByCategory($this->user->id);
+       $expenseSumsByCategory = Expenses::getSumsGroupedByCategory($this->user->id, $balancePeriod);
        
        $expensesSum = 0;
        
@@ -60,8 +82,8 @@ class Balance extends Authenticated
            'incomeSumsByCategory' => $incomeSumsByCategory,
            'expenseSumsByCategory' => $expenseSumsByCategory,
            'totalIncome' => $incomesSum,
-           'totalExpense' => $expensesSum
-           
+           'totalExpense' => $expensesSum,
+           'header' => $balanceHeader
        ]);
    
    }
